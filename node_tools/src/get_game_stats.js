9db5dec45fb00a978ff982a4f2898ca48de4f728
mzappitello <mzappitello@gmail.com>
2016-08-13 22:14:33 +0000

    prototype of get player stats up and running
    just spits out to console

--- node_tools/src/get_game_stats.js
+++ node_tools/src/get_game_stats.js
@@ -3,16 +3,34 @@
 /// Exports: get_player_stats
 "use strict";
 
-let nba_stats_request = require("nba");
+let nba_stats_request = require("nba").usePromises();
 
 // get the stats for a player
 // @player_name - the name for the player
 //
 // @returns -  [ { gameId: <Number>, teamId: <String>,  stats: <Stats> }]
-let get_player_stats = function (player_name) {
+let getPlayerStats = function (playerName) {
+  let player = nba_stats_request.findPlayer(playerName);
 
+  // if we couldn't find the player, pass an error to the callback and exit
+  if (!player === null) {
+    throw new Error("Unable to find player `" + playerName + '`.');
+  }
+
+  let request = {playerId: player.playerId};
+
+  // nba_stats_request.stats.playerInfo(request, function (err, response) {
+  nba_stats_request.stats.playerProfile(request, function (err, response) {
+    if (response) {
+      console.log(response);
+    }
+
+    if (err) {
+      console.log(err);
+    }
+  });
 };
 
-module.exports {
-  get_player_stats
+module.exports = {
+   getPlayerStats 
 };
