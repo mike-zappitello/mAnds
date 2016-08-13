8172c5506a0f3a9459c7a230509b61d2e2265428
mzappitello <mzappitello@gmail.com>
2016-08-13 23:21:09 +0000

    Start using Q module

--- node_tools/src/get_game_stats.js
+++ node_tools/src/get_game_stats.js
@@ -3,31 +3,34 @@
 /// Exports: get_player_stats
 "use strict";
 
-let nba_stats_request = require("nba").usePromises();
+let nbaDotCom = require("nba");
+let q = require('q');
 
 // get the stats for a player
 // @player_name - the name for the player
 //
-// @returns -  [ { gameId: <Number>, teamId: <String>,  stats: <Stats> }]
+// @returns -  [ { gameId: <Number>, team: <String>,  stats: <Stats> }]
 let getPlayerStats = function (playerName) {
-  let player = nba_stats_request.findPlayer(playerName);
+  let player = nbaDotCom.findPlayer(playerName);
 
   // if we couldn't find the player, pass an error to the callback and exit
   if (!player === null) {
     throw new Error("Unable to find player `" + playerName + '`.');
   }
 
-  let request = {playerId: player.playerId};
+  let requestOverrides = {playerId: player.playerId};
 
-  // nba_stats_request.stats.playerInfo(request, function (err, response) {
-  nba_stats_request.stats.playerProfile(request, function (err, response) {
-    if (response) {
-      console.log(response);
-    }
+  return q.nfapply(nbaDotCom.stats.playerProfile, [ requestOverrides ])
+  .then(function(playerData) {
+      // get the game ids 
+      let graphGameList = playerData.graphGameList;
+      let games = [];
 
-    if (err) {
-      console.log(err);
-    }
+      graphGameList.forEach(function(game) {
+          games.push( { gameId: game.gameId, team: game.teamAbbreviation } );
+      });
+
+      return games;
   });
 };
 
