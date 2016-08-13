8c195b4e5afeeffb661af9d6a3456c47b7a10b65
mzappitello <mzappitello@gmail.com>
2016-08-13 21:18:57 +0000

    add node_tools to extract data with

---
+++ node_tools/src/get_game_stats.js
@@ -0,0 +1,18 @@
+/// Create object storing all stats for a player in each game
+///
+/// Exports: get_player_stats
+"use strict";
+
+let nba_stats_request = require("nba");
+
+// get the stats for a player
+// @player_name - the name for the player
+//
+// @returns -  [ { gameId: <Number>, teamId: <String>,  stats: <Stats> }]
+let get_player_stats = function (player_name) {
+
+};
+
+module.exports {
+  get_player_stats
+};
